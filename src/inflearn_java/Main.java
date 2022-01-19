e8e870ff3277629782baa3549a46e04e11a52599
sung <ij16_21@naver.com>
2022-01-19 11:22:24 +0900

    구구단 method 이용 방식

--- src/inflearn_java/Main.java
+++ src/inflearn_java/Main.java
@@ -1,10 +1,27 @@
 package inflearn_java;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("20220118");
-		System.out.println("202");
-		System.out.println("201122");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int N =  scanner.nextInt();
+		
+		int[] arr = new int[9];
+		
+		gugu(N, arr);
+		
+	}
+
+	static void gugu(int N, int arr[]) {
+		
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = N * (i+1);
+			System.out.println(arr[i]);
+		}
+		
 	}
 }
