6cf6d48382a6a43149fc28ed27118f8161efa714
sung <ij16_21@naver.com>
2022-01-31 13:28:05 +0900

    일정-자동 달력 계산 포함-swithch문 이용-class분리

--- src/inflearn_java/Main.java
+++ src/inflearn_java/Main.java
@@ -1,120 +1,18 @@
 package inflearn_java;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Calendar;
-import java.util.HashMap;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		printMenu();
-		menu_choice();
-	}
+//	public static void main(String[] args) throws IOException {
+//		PlanItem.printMenu();
+//		PlanItem.menu_choice();
+//	}
 	
-	
-	public static void printMenu() {
-		System.out.println("+----------------------+");
-		System.out.println("| 1. 일정 등록");
-		System.out.println("| 2. 일정 검색");
-		System.out.println("| 3. 달력 보기");
-		System.out.println("| h. 도움말 q. 종료");
-		System.out.println("+----------------------+");
-	}
-	
-	
-	public static void menu_choice() throws IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashMap<String, String> map = new HashMap<String, String>();
-		String N;
-		do {
-			System.out.println("명령 (1, 2, 3, h, q)");
-			System.out.print(">");
-			N = br.readLine();
-			
-//			if(N.equals("1")) {
-//				System.out.println("[일정 등록] 날짜를 입력하세요.");
-//				System.out.print(">");
-//				String day = br.readLine();
-//				System.out.println("일정을 입력하세요");
-//				System.out.print(">");
-//				String work = br.readLine();
-//				map.put(day, work);
-//				System.out.println("일정이 등록되었습니다");
-//			}
-//			else if(N.equals("2")) {
-//				System.out.println("[일정 검색] 날짜를 입력하세요");
-//				System.out.print(">");
-//				String checkday = br.readLine();
-//				if(map.containsKey(checkday)) {
-//					System.out.println("1개의 일정이 있습니다");
-//					System.out.println(map.get(checkday));
-//				}
-//				else {
-//					System.out.println("일정이 존재하지 않습니다");
-//				}
-//				
-//			}
-//			else if(N.equals("3")) {
-//				System.out.println("년도를 입력해주세요");
-//				System.out.print("YEAR>");
-//				String year = br.readLine();
-//				System.out.println("달을 입력하세요");
-//				System.out.print("MONTH>");
-//				String month = br.readLine();
-//				findleapyear(year, month);
-//				
-//				
-//			}
-//			else if(N.equals("h")) {
-//				printMenu();
-//			}
-			switch(N) {
-			case "1":
-				System.out.println("[일정 등록] 날짜를 입력하세요.");
-				System.out.print(">");
-				String day = br.readLine();
-				System.out.println("일정을 입력하세요");
-				System.out.print(">");
-				String work = br.readLine();
-				map.put(day, work);
-				System.out.println("일정이 등록되었습니다");
-				break;
-			case "2":
-				System.out.println("[일정 검색] 날짜를 입력하세요");
-				System.out.print(">");
-				String checkday = br.readLine();
-				if(map.containsKey(checkday)) {
-					System.out.println("1개의 일정이 있습니다");
-					System.out.println(map.get(checkday));
-				}
-				else {
-					System.out.println("일정이 존재하지 않습니다");
-				}
-				break;
-			case "3":
-				System.out.println("년도를 입력해주세요");
-				System.out.print("YEAR>");
-				String year = br.readLine();
-				System.out.println("달을 입력하세요");
-				System.out.print("MONTH>");
-				String month = br.readLine();
-				findleapyear(year, month);
-				break;
-			case "h":
-				printMenu();
-				break;
-			}
-			
-			
-		}while(!N.equals("q"));
-		System.out.println("Bye");
-	}
+	//윤년 찾는 알고리즘
 	static void findleapyear(String year, String month) {
-		
 		int lastday = 0;
 		int number = Integer.parseInt(month);
-		
 		if(month.equals("2")) {
 			if(Integer.parseInt(year) % 4 == 0) {
 				if( Integer.parseInt(year) % 400 == 0) {
@@ -154,6 +52,7 @@ public class Main {
 	}
 	
 	
+	//달력 찾는 알고리즘
 	static void findcal(String year, String month, int lastday) {
 		
 		int count = 0;
